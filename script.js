590a3baf13afa883d5f2e308913aa0e6885a5e52
Julian Herlein <julian.n.herlein@accenture.com>
2022-02-01 11:39:23 -0300

    added some websockets connections

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 let number = document.getElementById("input-1");
 let count = 0
+let wsBtc = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@ticker');
 
-function addNumber() {
-    count += 1
-    number.value = count;
-
+function getETHPrice() {
+    let ws = new WebSocket('wss://stream.binance.com:9443/ws/ethusdt@ticker');
+    ws.onmessage = (event) => {
+        let cryptoObject = JSON.parse(event.data);
+        let price = cryptoObject.c;
+        number.value = "ETH USD " + price //+ Math.trunc(price);
+        console.log("ETH " + price);
+        ws.close();
+        console.log("Connection close");     
+    };
 }
+
+wsBtc.onmessage = (event) => {
+    let cryptoObject = JSON.parse(event.data);
+    let price = cryptoObject.c
+    //number.value = price
+    console.log("BTC " + price)
+    
+};
+
