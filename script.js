fc424e725caf961ff374b1c341ef04a0f2f7fd9a
Julian Herlein <julian.n.herlein@accenture.com>
2022-02-01 14:14:30 -0300

    added arrow up/down

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
-let number = document.getElementById("input-1");
-let count = 0
+let ethElement = document.getElementById("eth-price-1");
+let btcElement = document.getElementById("btc-price-1");
+let arrowElement_1 = document.getElementById("arrow-up-down-1");
+let arrowElement_2 = document.getElementById("arrow-up-down-2");
+
+let count = 0;
 let wsBtc = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@ticker');
+let lastEthPrice = 2748;
+let lastBtcPrice = 30000;
+
+
+function comparePrice(price, lastPrice, htmlElement){
+    if (price < lastPrice){
+        console.log("Bajo")
+        htmlElement.style.backgroundColor = '#cf5353';
+    }
+    else{
+        console.log("Subio")
+        htmlElement.style.backgroundColor = '#53cf57'
+    };
+     
+}
 
 function getETHPrice() {
     let ws = new WebSocket('wss://stream.binance.com:9443/ws/ethusdt@ticker');
+    ws.onopen = function(){
+        console.log("Se conectó");
+    };
     ws.onmessage = (event) => {
         let cryptoObject = JSON.parse(event.data);
         let price = cryptoObject.c;
-        number.value = "ETH USD " + price //+ Math.trunc(price);
+        ethElement.innerText = "ETH Price USD " + parseFloat(price).toFixed(2);
         console.log("ETH " + price);
         ws.close();
-        console.log("Connection close");     
+        console.log("Connection close");
+        comparePrice(price, lastEthPrice,arrowElement_1)
+        lastEthPrice = price;
+            
     };
 }
 
 wsBtc.onmessage = (event) => {
     let cryptoObject = JSON.parse(event.data);
-    let price = cryptoObject.c
-    //number.value = price
-    console.log("BTC " + price)
+    let price = cryptoObject.c;
+    btcElement.innerText = "BTC Price USD " + parseFloat(price).toFixed(2);
+    console.log("BTC " + parseFloat(price));
+    comparePrice(price,lastBtcPrice,arrowElement_2)
+    lastBtcPrice = price
     
 };
 
+var intervalId = setInterval(getETHPrice, 2000);
